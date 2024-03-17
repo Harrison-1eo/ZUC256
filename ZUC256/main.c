37e29d962078ed5a043397efff556e41cd56fd95
harrison <lihc1314i@126.com>
2024-03-17 14:54:56 +0800

    Initial Commit

---
+++ ZUC256/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  ZUC256
+//
+//  Created by Harrison Lee on 2024/3/17.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
